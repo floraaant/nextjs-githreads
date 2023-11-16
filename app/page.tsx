71b2809d4e445ac49de2138da04026ad5edceb4f
Florant Hauguel <floraaant@macbook-pro-de-florant.home>
2023-11-16 22:58:28 +0100

    Initialisation Application

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { getAuthSession } from "@/lib/auth";
+
+export default async function Home() {
+  const session = await getAuthSession();
+  return (
+    <div>
+      <p>{JSON.stringify(session, null, 2)}</p>
+      <Button>Click me</Button>
+      <Input />
+    </div>
+  )
+}
---
+++ src/feature/layout/header.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/src/theme/ThemeToggle";
+import React from "react";
+import { LoginButton } from "./auth/LoginButton";
+import { getAuthSession } from "@/lib/auth";
+import { UserProfile } from "./auth/UserProfile";
+
+export const Header = async () => {
+    const session = await getAuthSession();
+
+    return(
+        <header className="border-b border-b-accent fixed top-0 bg-background w-full">
+            <div className="container flex items-centers py-2 max-w-lg m-auto gap-1">
+                <h2 className="text-2xl font-bold mr-auto">
+                    Githread
+                </h2>
+                {session?.user ? (<UserProfile />) : (<LoginButton />)}
+                <ThemeToggle />
+            </div>
+        </header>
+    )
+}
